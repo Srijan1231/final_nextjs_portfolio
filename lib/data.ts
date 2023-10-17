79a6ea19354bcc253b001c1112620f0d668ff1f1
srijan <srijan.dhl21@gmail.com>
2023-10-17 20:49:08 +1100

    projects with link

---
+++ lib/data.ts
@@ -0,0 +1,131 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import store from "@/public/ecom-store.png";
+import cms from "@/public/CMS.png";
+import nottodo from "@/public/not-todo.png";
+import expense from "@/public/expense.png";
+import calculator from "@/public/calculator.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  // {
+  //   name: "Experience",
+  //   hash: "#experience",
+  // },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+// export const experiencesData = [
+//   {
+//     title: "Graduated bootcamp",
+//     location: "Miami, FL",
+//     description:
+//       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
+//     icon: React.createElement(LuGraduationCap),
+//     date: "2019",
+//   },
+//   {
+//     title: "Front-End Developer",
+//     location: "Orlando, FL",
+//     description:
+//       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+//     icon: React.createElement(CgWorkAlt),
+//     date: "2019 - 2021",
+//   },
+//   {
+//     title: "Full-Stack Developer",
+//     location: "Houston, TX",
+//     description:
+//       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+//     icon: React.createElement(FaReact),
+//     date: "2021 - present",
+//   },
+// ] as const;
+
+export const projectsData = [
+  {
+    title: "E-commerce Store(IceStrike)",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Express", "MongoDB", "Tailwind", "Node.js","JWT","Redux","Stripe"],
+    imageUrl: store,
+    github_url:'https://github.com/Srijan1231/ecomm_store_frontend',
+    live_url:"http://icestrike-frontend-store.s3-website-ap-southeast-2.amazonaws.com/",
+  },
+  {
+    title: "Content Management System(CMS)",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "Express", "MongoDB", "Bootstrap", "Node.js","JWT","Redux","chartjs"],
+    imageUrl: cms,
+    github_url:"https://github.com/Srijan1231/Snowgear_Ecomm_CMS_frontend",
+    live_url:"http://icestrike-ecomm.s3-website-ap-southeast-2.amazonaws.com/" 
+  },
+  {
+    title: "Expense Tracker",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "Firebase", "Tailwind","chartjs"],
+    imageUrl: expense,
+    github_url:"https://github.com/Srijan1231/expense_tracker_nextjs",
+    live_url:"https://expense-tracker-nextjs-five.vercel.app/",
+  },
+  {
+    title: "Not-To Do App",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React","Express","MongoDB","Node.js"],
+    imageUrl: nottodo,
+    github_url:"https://github.com/Srijan1231/not-to-do-list",
+    live_url:"https://to-do-list-basic-self.vercel.app/",
+  },
+  {
+    title: "Calculator",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React","Express","MongoDB","Node.js"],
+    imageUrl: calculator,
+    github_url:"https://github.com/Srijan1231/react-calc",
+    live_url:"https://react-calc-gray.vercel.app/",
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "JWT",
+  "Express",
+  "Authentication",
+  " Authorization",
+  "Stripe Integration"
+  
+] as const;
