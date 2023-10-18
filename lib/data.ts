8d8b1593ac8dfe18e715b5e09e1220810cc980ce
srijan <srijan.dhl21@gmail.com>
2023-10-18 12:52:51 +1100

    despriction change

--- lib/data.ts
+++ lib/data.ts
@@ -66,7 +66,7 @@ export const projectsData = [
   {
     title: "E-commerce Store(IceStrike)",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+      "Elevated e-commerce: MERN, CMS, secure, and optimized for seamless shopping.",
     tags: ["React", "Express", "MongoDB", "Tailwind", "Node.js","JWT","Redux","Stripe"],
     imageUrl: store,
     github_url:'https://github.com/Srijan1231/ecomm_store_frontend',
@@ -75,7 +75,7 @@ export const projectsData = [
   {
     title: "Content Management System(CMS)",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+      "Tailored MERN CMS streamlines e-commerce: dynamic product management, secure payments, and business growth.",
     tags: ["React", "Express", "MongoDB", "Bootstrap", "Node.js","JWT","Redux","chartjs"],
     imageUrl: cms,
     github_url:"https://github.com/Srijan1231/Snowgear_Ecomm_CMS_frontend",
@@ -84,7 +84,7 @@ export const projectsData = [
   {
     title: "Expense Tracker",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      "An expense tracker app built with Next.js and Firebase, providing secure authentication and real-time database updates for efficient financial management.",
     tags: ["React", "Next.js", "Firebase", "Tailwind","chartjs"],
     imageUrl: expense,
     github_url:"https://github.com/Srijan1231/expense_tracker_nextjs",
@@ -93,7 +93,7 @@ export const projectsData = [
   {
     title: "Not-To Do App",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      "A MERN-based not-to-do app that helps users track and avoid unproductive or undesirable tasks and behaviors with intuitive features.",
     tags: ["React","Express","MongoDB","Node.js"],
     imageUrl: nottodo,
     github_url:"https://github.com/Srijan1231/not-to-do-list",
@@ -102,7 +102,7 @@ export const projectsData = [
   {
     title: "Calculator",
     description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+      "A MERN-based calculator app for performing basic arithmetic operations with a user-friendly interface.",
     tags: ["React","Express","MongoDB","Node.js"],
     imageUrl: calculator,
     github_url:"https://github.com/Srijan1231/react-calc",
