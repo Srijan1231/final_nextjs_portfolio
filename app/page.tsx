283fa17f2b6e5bfbab4478a433a38bec49b87e95
srijan <srijan.dhl21@gmail.com>
2024-01-24 21:47:01 +1100

    added experience section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import About from "@/components/about";
 import Contact from "@/components/contact";
+import Experience from "@/components/experience";
 // import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
@@ -14,7 +15,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
-      {/* <Experience /> */}
+      <Experience />
       <Contact />
     </main>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -25,88 +25,81 @@ export const links = [
     name: "Skills",
     hash: "#skills",
   },
-  // {
-  //   name: "Experience",
-  //   hash: "#experience",
-  // },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
   {
     name: "Contact",
     hash: "#contact",
   },
 ] as const;
 
-// export const experiencesData = [
-//   {
-//     title: "Graduated bootcamp",
-//     location: "Miami, FL",
-//     description:
-//       "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-//     icon: React.createElement(LuGraduationCap),
-//     date: "2019",
-//   },
-//   {
-//     title: "Front-End Developer",
-//     location: "Orlando, FL",
-//     description:
-//       "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-//     icon: React.createElement(CgWorkAlt),
-//     date: "2019 - 2021",
-//   },
-//   {
-//     title: "Full-Stack Developer",
-//     location: "Houston, TX",
-//     description:
-//       "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-//     icon: React.createElement(FaReact),
-//     date: "2021 - present",
-//   },
-// ] as const;
+export const experiencesData = [
+  {
+    title: "Dented Code, Software Development Bootcamp",
+    location: "Sydney, NSW",
+    description:
+      "I graduated after 6 months of studying",
+    icon: React.createElement(LuGraduationCap),
+    date: "2023",
+  },
+  {
+    title: "Software Engineering Internship",
+    location: "Sydney, NSW",
+    description:
+      "Developed and maintained responsive web apps with React, Next.js, and Zustand, collaborating seamlessly with backend developers to integrate RESTful APIs, while implementing Jest for unit tests and prioritizing continuous learning and code documentation.",
+    icon: React.createElement(CgWorkAlt),
+    date: "October-2023",
+  },
+
+] as const;
 
 export const projectsData = [
   {
     title: "E-commerce Store(IceStrike)",
     description:
       "Elevated e-commerce: MERN, CMS, secure, and optimized for seamless shopping.",
-    tags: ["React", "Express", "MongoDB", "Tailwind", "Node.js","JWT","Redux","Stripe"],
+    tags: ["React", "Express", "MongoDB", "Tailwind", "Node.js", "JWT", "Redux", "Stripe"],
     imageUrl: store,
-    github_url:'https://github.com/Srijan1231/ecomm_store_frontend',
-    live_url:"http://icestrike-frontend-store.s3-website-ap-southeast-2.amazonaws.com/",
+    github_url: 'https://github.com/Srijan1231/ecomm_store_frontend',
+    live_url: "http://icestrike-frontend-store.s3-website-ap-southeast-2.amazonaws.com/",
   },
   {
     title: "Content Management System(CMS)",
     description:
       "Tailored MERN CMS streamlines e-commerce: dynamic product management, secure payments, and business growth.",
-    tags: ["React", "Express", "MongoDB", "Bootstrap", "Node.js","JWT","Redux","chartjs"],
+    tags: ["React", "Express", "MongoDB", "Bootstrap", "Node.js", "JWT", "Redux", "chartjs"],
     imageUrl: cms,
-    github_url:"https://github.com/Srijan1231/Snowgear_Ecomm_CMS_frontend",
-    live_url:"http://icestrike-ecomm.s3-website-ap-southeast-2.amazonaws.com/" 
+    github_url: "https://github.com/Srijan1231/Snowgear_Ecomm_CMS_frontend",
+    live_url: "http://icestrike-ecomm.s3-website-ap-southeast-2.amazonaws.com/"
   },
   {
     title: "Expense Tracker",
     description:
       "An expense tracker app built with Next.js and Firebase, providing secure authentication and real-time database updates for efficient financial management.",
-    tags: ["React", "Next.js", "Firebase", "Tailwind","chartjs"],
+    tags: ["React", "Next.js", "Firebase", "Tailwind", "chartjs"],
     imageUrl: expense,
-    github_url:"https://github.com/Srijan1231/expense_tracker_nextjs",
-    live_url:"https://expense-tracker-nextjs-five.vercel.app/",
+    github_url: "https://github.com/Srijan1231/expense_tracker_nextjs",
+    live_url: "https://expense-tracker-nextjs-five.vercel.app/",
   },
   {
     title: "Not-To Do App",
     description:
       "A MERN-based not-to-do app that helps users track and avoid unproductive or undesirable tasks and behaviors with intuitive features.",
-    tags: ["React","Express","MongoDB","Node.js"],
+    tags: ["React", "Express", "MongoDB", "Node.js"],
     imageUrl: nottodo,
-    github_url:"https://github.com/Srijan1231/not-to-do-list",
-    live_url:"https://to-do-list-basic-self.vercel.app/",
+    github_url: "https://github.com/Srijan1231/not-to-do-list",
+    live_url: "https://to-do-list-basic-self.vercel.app/",
   },
   {
     title: "Calculator",
     description:
       "A MERN-based calculator app for performing basic arithmetic operations with a user-friendly interface.",
-    tags: ["React","Express","MongoDB","Node.js"],
+    tags: ["React", "Express", "MongoDB", "Node.js"],
     imageUrl: calculator,
-    github_url:"https://github.com/Srijan1231/react-calc",
-    live_url:"https://react-calc-gray.vercel.app/",
+    github_url: "https://github.com/Srijan1231/react-calc",
+    live_url: "https://react-calc-gray.vercel.app/",
   },
 ] as const;
 
@@ -127,5 +120,5 @@ export const skillsData = [
   "Authentication",
   " Authorization",
   "Stripe Integration"
-  
+
 ] as const;
