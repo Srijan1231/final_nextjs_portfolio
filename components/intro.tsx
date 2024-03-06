5ff38dc2f3d3f38ac5ebf12a89997e1fa2e412bf
srijan <srijan.dhl21@gmail.com>
2024-03-06 11:00:12 +1100

    .

--- components/intro.tsx
+++ components/intro.tsx
@@ -91,7 +91,7 @@ export default function Intro() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-          href="/Srijan_Dahal_Resume-sydney.docx"
+          href="/Srijan_Dahal_Resume_Sydney_software.docx"
           download
         >
           Download CV{" "}
